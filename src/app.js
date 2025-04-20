b4c5627d7e90baa32b7630fd0d29c44f049c6f26
Oleksandr Kovalchuk <kov280396@gmail.com>
2025-04-20 19:14:51 +0300

    implemented a moveFile function

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-// write code here
+/* eslint-disable no-console */
+
+const fs = require('fs/promises');
+const path = require('path');
+
+const [sourceFilePath, destinationInputPath] = process.argv.slice(2);
+
+async function moveFile(sourcePath, destinationPath) {
+  try {
+    const destinationStats = await fs.stat(destinationPath).catch(() => null);
+
+    const destinationIsDirectory =
+      destinationStats && destinationStats.isDirectory();
+
+    const finalDestinationPath = destinationIsDirectory
+      ? path.join(destinationPath, path.basename(sourcePath))
+      : destinationPath;
+
+    await fs.rename(sourcePath, finalDestinationPath);
+  } catch (error) {
+    console.error('Failed to move file:', error.message);
+  }
+}
+
+moveFile(sourceFilePath, destinationInputPath);
